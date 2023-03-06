d4c619358537d5e99d0cfa2ad5de2db25b1b5b2c
Faustas Linkevičius <faulin@ktu.lt>
2023-03-06 21:16:12 +0200

    Only allowing the logged in user to access the main page

--- Blueprints/main.py
+++ Blueprints/main.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect
+from database import mysql
+import Models.user
 
 mainBlueprint = Blueprint('mainBlueprint', __name__, template_folder="../templates", static_folder="../static")
 
-@mainBlueprint.route('/pagrindinis')
+@mainBlueprint.route('/pagrindinis', methods = ["GET", "POST"])
 def index():
+    if "user" not in session:
+        return redirect("../", code = 302)
     return render_template("pagrindinis.html")
