314d75de5d4162f0865df2b4b537cd6269a7ea5d
Faustas Linkevičius <faulin@ktu.lt>
2023-03-08 22:58:38 +0200

    Duomenų konvertavimas į svg

--- Tools/JsonToSvg/Converter.py
+++ Tools/JsonToSvg/Converter.py
@@ -1,18 +1,22 @@
 import json
 
-fileName = "polygons.txt"
+fileName = "polygons.html"
 
-def readFile(fileName):
+def ReadFile(fileName):
     f = open("districts.json", encoding="utf-8")
     data = json.load(f)
     f.close()
     ans = []
     for i in range(len(data['features'])):
-        ans.append(data['features'][i]['geometry']['coordinates'][0][0])
+        if i != 2:
+            ans.append(data['features'][i]['geometry']['coordinates'][0][0])
+        else:
+            ans.append(data['features'][i]['geometry']['coordinates'][1][0])
+            ans.append(data['features'][i]['geometry']['coordinates'][3][0])
     return ans
 
-def findMinMax(data):
-    X = [100,0]
+def FindMinMax(data):
+    X = [100, 0]
     Y = [100, 0]
     for i in data:
         for i2 in i:
@@ -27,22 +31,57 @@ def findMinMax(data):
                 Y[1] = i2[1]
     return [X, Y]
 
-def toRange(data, minMax, trueMax):
+def ToRange(data, minMax, trueMax):
     xRange = minMax[0][1] - minMax[0][0]
     yRange = minMax[1][1] - minMax[1][0]
 
-    if xRange > yRange:
-        yMax = trueMax * yRange / xRange
-        xMax = trueMax
-    else:
-        xMax = trueMax * xRange / yRange
-        yMax = trueMax
+    xMax = trueMax
+    yMax = yRange / xRange * trueMax * 1.75
     ans = []
     for i in range(len(data)):
         ans.append([])
         for i2 in data[i]:
             ans[i].append([ (i2[0] - minMax[0][0]) / ( minMax[0][1] - minMax[0][0]) * xMax, (i2[1] - minMax[1][0]) / ( minMax[1][1] - minMax[1][0]) * yMax])
+    return ans, [xMax, yMax]
+
+def Invert(data, max):
+    ans = []
+    for i in data:
+        temp = []
+        for i2 in i:
+            temp.append([i2[0], max - i2[1]])
+        ans.append(temp)
     return ans
 
-data = readFile(fileName)
-print(toRange(data, findMinMax(data), 1000))
+
+def ToSVG(data):
+    ans = "<svg height=\"100%\" width=\"100%\">\n"
+
+    for i in range(len(data)):
+        ans += "\t <g style=\"stroke:purple;stroke-width:3;fill:none\">\n"
+        if i != 2:
+
+            ans += "\t\t <polygon points=\""
+            for i2 in data[i]:
+                ans += str(i2[0]) + "," + str(i2[1]) + " "
+            ans += "\" />\n"
+        else:
+            for temp in range(2):
+                ans += "\t\t <polygon points=\""
+                for i2 in data[i + temp]:
+                    ans += str(i2[0]) + "," + str(i2[1]) + " "
+                ans += "\" />\n"
+            i += 1
+        ans += "\t</g>\n"
+    ans += "</svg>"
+    return ans
+
+def ToFile(ans, fileName):
+    f = open(fileName, "w")
+    f.write(ans)
+    f.close()
+
+data = ReadFile(fileName)
+coords, maxRange = ToRange(data, FindMinMax(data), 1000)
+coords = Invert(coords, maxRange[1])
+ToFile(ToSVG(coords), fileName)
