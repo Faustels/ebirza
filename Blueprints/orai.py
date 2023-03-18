8e03ebeae7cb8b233e565a1fa70e08189c9d1203
Faustas Linkevičius <faulin@ktu.lt>
2023-03-18 18:04:15 +0200

    Saules valandu atvaizdavimas

--- Blueprints/orai.py
+++ Blueprints/orai.py
@@ -1,9 +1,17 @@
 from flask import Blueprint, render_template, session, redirect
-from database import mysql
+from database import MySQLGet
 import Models.user
 
 oraiBlueprint = Blueprint('oraiBlueprint', __name__, template_folder="../templates", static_folder="../static")
 
+def ToJSDictionary(data):
+    ans = "const sunHours = {\n"
+    for i in data:
+        ans += i["name"] + ": " + str(i["sunshineHours"]) + ",\n"
+    ans += "};"
+    return(ans)
+
 @oraiBlueprint.route('/orai')
 def index():
-    return render_template("orai.html")
+    sun = MySQLGet('Select name, sunshineHours FROM county')
+    return render_template("orai.html", sunShineDict = ToJSDictionary(sun))
--- templates/inserts/map/map.js
+++ templates/inserts/map/map.js
@@ -1,37 +1,66 @@
 
 document.addEventListener("mousemove", changeTooltip);
 currentSelector = ""
+tooltip = $("#customTooltip")[0]
+mapFront = $("#mapFrontRegion")[0]
+SetColors(sunHours)
 function toFront(object){
-    $("#mapFrontRegion")[0].classList.remove("mapGSelected")
-    if (object.classList.contains("mapGSelected")) {
-        $("#mapFrontRegion")[0].classList.add("mapGSelected")
-    }
-    $("#mapFrontRegion")[0].innerHTML = object.innerHTML
-    $("#mapFrontRegion")[0].tooltipText = "<b>" + object.id + "</b>"
-    $("#mapFrontRegion")[0].currentlySelected = object.id
+    mapFront.innerHTML = object.innerHTML
+    mapFront.currentlyFront = object.id
+    mapFront.tooltipText = "<b>" + object.id + "</b>" + "<p>" + sunHours[object.id].toString() + "val. per metus" + "</p>"
     showTooltip(object)
 }
 function showTooltip(object) {
-    $("#customTooltip")[0].style.display = 'block'
-    $("#customTooltip")[0].innerHTML = object.tooltipText
+    tooltip.style.display = 'block'
+    tooltip.innerHTML = object.tooltipText
 }
 function hideTooltip(){
-    $("#customTooltip")[0].style.display = 'none'
+    tooltip.style.display = 'none'
 }
 function changeTooltip(e){
-    $("#customTooltip")[0].style.top = (e.clientY).toString() + "px"
-    $("#customTooltip")[0].style.left = (e.clientX + 20).toString()  + "px"
+    tooltip.style.top = (e.clientY).toString() + "px"
+    tooltip.style.left = (e.clientX + 20).toString()  + "px"
 }
 
 function clickRegion(object) {
-    if (currentSelector == object.currentlySelected) {
-        $("#" + object.currentlySelected)[0].classList.remove("mapGSelected")
+    if (currentSelector === object.currentlyFront) {
+        $("#" + object.currentlyFront)[0].classList.remove("mapGSelected")
         currentSelector = ""
     } else {
-        if (currentSelector != "") {
+        if (currentSelector !== "") {
             $("#" + currentSelector)[0].classList.remove("mapGSelected")
         }
-        $("#" + object.currentlySelected)[0].classList.add("mapGSelected")
-        currentSelector = object.currentlySelected
+        $("#" + object.currentlyFront)[0].classList.add("mapGSelected")
+        currentSelector = object.currentlyFront
     }
+}
+
+function SetColors(values) {
+    let max = 0
+    let min = 366 * 24
+
+    for (let key in values) {
+        if (values[key] < min) {
+            min = values[key]
+        }
+        if (values[key] > max) {
+            max = values[key]
+        }
+    }
+
+    for (let key in values) {
+        $("#" + key)[0].style.fill = getColor((values[key] - min) / (max - min))
+    }
+}
+
+function getColor(amount) {
+    let maxColorG = 90
+    let maxColorB = 85
+    let minColorG = 225
+    let minColorB = 160
+
+
+    let ats = "#F5" + Math.round(minColorG - (minColorG - maxColorG) * amount).toString(16)
+        + Math.round(minColorB - (minColorB - maxColorB) * amount).toString(16)
+    return ats
 }
