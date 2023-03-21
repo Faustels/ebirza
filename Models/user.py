30d63b86ec3ce9044c1295da2ef09c2f677d88cd
Faustas Linkevičius <faulin@ktu.lt>
2023-03-21 10:43:10 +0200

    Atvaizduojama vartotojo vieta

--- Models/user.py
+++ Models/user.py
@@ -1,5 +1,9 @@
+from Services.Location.autocomplete import GetAutocomplete
 class User:
   def __init__(self, id, email, address):
     self.id = id
     self.email = email
     self.address = address
+    temp = GetAutocomplete(address).json()
+    self.latitude = temp["results"][0]["lat"]
+    self.longitude = temp["results"][0]["lon"]
