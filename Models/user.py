faf476a29dd70188b383a44d4ad51276ac628efe
Faustas Linkevičius <faulin@ktu.lt>
2023-04-27 11:55:17 +0300

    Neieškomos koordinatės, jei vartotojo adresas yra blogas.

--- Models/user.py
+++ Models/user.py
@@ -4,9 +4,12 @@ class User:
     self.id = id
     self.email = email
     self.address = address
+    self.latitude = None
+    self.longitude = None
     temp = GetLocations(address).json()
-    self.latitude = temp["results"][0]["lat"]
-    self.longitude = temp["results"][0]["lon"]
+    if len(temp) != 0:
+      self.latitude = temp["results"][0]["lat"]
+      self.longitude = temp["results"][0]["lon"]
     self.produced = produced
     self.setPrice = setPrice
     self.consumed = consumed
