a17e87c514cb41f720e4d9d1a0101189659427d9
Faustas Linkevičius <faulin@ktu.lt>
2023-05-08 19:04:32 +0300

    Pirkimo funkcija

--- Blueprints/main.py
+++ Blueprints/main.py
@@ -1,5 +1,9 @@
 from flask import Blueprint, render_template, session, redirect, request
 from Services.Database.database import MySQLGet, MySQLExecute
+import threading
+
+lock = threading.Lock()
+moneyTaken = 0.02
 
 mainBlueprint = Blueprint('mainBlueprint', __name__, template_folder="../templates", static_folder="../static")
 
@@ -44,7 +48,7 @@ def getData():
     price = float(request.args.get("price"))
     amount = int(request.args.get("amount"))
 
-    res = MySQLGet("select name, lastName, price, amount from producer inner join user on user.producer = producer.id" +
+    res = MySQLGet("select name, lastName, price, amount, producer.id as id from producer inner join user on user.producer = producer.id" +
                           " where price > 0 and amount > 0 and price < %s and amount < %s", (price, amount))
 
     if len(res) == 0:
@@ -56,6 +60,7 @@ def getData():
         tempRes["name"] = user["name"] + " " + user["lastName"]
         tempRes["price"] = user["price"]
         tempRes["amount"] = user["amount"]
+        tempRes["id"] = user["id"]
         result.append(tempRes)
 
     return result
@@ -67,12 +72,34 @@ def buyEnergy():
     if session["user"].consumed == None:
         return [None]
 
+    lock.acquire()
+
     producerId = int(request.get_json())
-    consumed = session["user"].consumed
-    res = MySQLGet("select id from producer, amount where id = %s", (producerId,))
+    res = MySQLGet("select id from producer, amount, price where id = %s", (producerId,))
 
     if len(res) == 0:
+        lock.release()
         return [None]
+
+    amountBought = res[0]["amount"]
+    price = amountBought * res[0]["price"]
+    if amountBought <= 0:
+        lock.release()
+        return [None]
+
+    MySQLExecute("update user set balance = balance + %s, amount = 0 where producer = %s",
+                 (price - amountBought * moneyTaken, producerId))
+
+    MySQLExecute("update user set balance = balance - %s where id = %s", (price, session["user"]["id"]))
+
+    consumerId = MySQLGet("select consumer from user where id = %s", (session["user"]["id"],))
+
+    MySQLExecute("update consumer set amount = amount + %s where id = %s", (amountBought, consumerId))
+
+    lock.release()
+
+    return [True]
+
 
 
 
