a14ba04c4a543b2129beeedd780a181622d83dfb
Faustas Linkevičius <faulin@ktu.lt>
2023-05-20 18:10:19 +0300

    pakeistas apiemus linkas

--- Blueprints/about.py
+++ Blueprints/about.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, session
 
 aboutBlueprint = Blueprint('aboutBlueprint', __name__, template_folder="../templates", static_folder="../static")
 
-@aboutBlueprint.route('/ApieMus')
+@aboutBlueprint.route('/apiemus')
 def index():
     if session.get("user"):
         return render_template("about.html", user=session["user"])
