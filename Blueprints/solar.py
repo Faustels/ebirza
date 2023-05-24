36c01b715c995398d13693d669823e677f39db41
Faustas Linkevičius <faulin@ktu.lt>
2023-05-24 22:24:11 +0300

    Spinduliuotes langas

--- Blueprints/solar.py
+++ Blueprints/solar.py
@@ -1,7 +1,6 @@
-from flask import Blueprint, render_template, session
-from Services.Database.database import MySQLGet
+from flask import Blueprint, render_template
 solarBlueprint = Blueprint('solarBlueprint', __name__, template_folder="../templates", static_folder="../static")
 
 @solarBlueprint.route('/spinduliuote')
 def index():
-    return render_template("saule.html",)
+    return render_template("orai.html")
